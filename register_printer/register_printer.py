f9ebf36d1f581de1855be711bab367390d8fbc07
Yi Zhang <zhangyiant@hotmail.com>
2019-08-17 14:28:22 +0800

    Add RegisterPrinter class.

---
+++ register_printer/register_printer.py
@@ -0,0 +1,21 @@
+class RegisterPrinter:
+    def __init__(self, config_file, excel_path):
+        return
+
+    def display(self):
+        return
+
+    def parse(self, config_file, excel_path):
+        return
+
+    def generate_uvm(self):
+        return
+
+    def generate_rtl(self):
+        return
+
+    def generate_c_header(self):
+        return
+
+    def generate_document(self):
+        return
