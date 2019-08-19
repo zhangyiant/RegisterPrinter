257d5dbfc485efe99e66f9c69a5a04a336d53570
Yi Zhang <zhangyiant@hotmail.com>
2019-08-19 21:01:54 +0800

    Extract validate function.

--- register_printer/parse_excels.py
+++ register_printer/parse_excels.py
@@ -10,161 +10,167 @@ from xlrd import *
 
 LOGGER = logging.getLogger(__name__)
 
+def validate_sheet(sheet):
+    if sheet.ncols < 8:
+        LOGGER.error(
+            "sheet %s error: column number is wrong, must be greater than 8",
+            sheet.name)
+        raise Exception("Sheet column number must be geater than 8")
+
+    if sheet.cell(0, 0).value != "Module description:":
+        LOGGER.debug(
+            "sheet %s error: find no \"Module description:\" in cell(0,0)",
+            sheet.name)
+        raise Exception("No \"Module description:\" in cell(0,0)")
+    return
+
+
 def process_sheet(sheet, block):
     LOGGER.debug(
         "Processing sheet %s row=%d col=%d",
         sheet.name,
         sheet.nrows,
         sheet.ncols)
-    if sheet.ncols < 8:
-        LOGGER.error(
-            "sheet %s error: column number is wrong, must be greater than 8",
-            sheet.name)
-        raise Exception("Sheet column number must be geater than 8")
-    else:
-        if sheet.cell(0, 0).value != "Module description:":
-            LOGGER.debug(
-                "sheet %s error: find no \" Module description\" in cell(0,0)",
-                filename,
-                sheet.name)
-        row = 3
-        while row < sheet.nrows:
-            if re.match(r'0x', str(sheet.cell(row, 0).value)):
-                offset = int(sheet.cell(row, 0).value, 16)
-                if offset > block.size:
-                    LOGGER.error(
-                        "sheet %s row %d error: offset %x > block size %x",
-                        filename,
-                        sheet.name,
-                        row,
-                        offset,
-                        block.size)
-                    raise Exception("offset > block size")
-                name = sheet.cell(row, 1).value
+
+    validate_sheet(sheet)
+
+    row = 3
+    while row < sheet.nrows:
+        if re.match(r'0x', str(sheet.cell(row, 0).value)):
+            offset = int(sheet.cell(row, 0).value, 16)
+            if offset > block.size:
+                LOGGER.error(
+                    "sheet %s row %d error: offset %x > block size %x",
+                    sheet.name,
+                    row,
+                    offset,
+                    block.size)
+                raise Exception("offset > block size")
+            name = sheet.cell(row, 1).value
+            description = "%s" % (sheet.cell(row, 7).value)
+            register = Register(name, offset, description)
+            block.add_register(register)
+            if sheet.cell(row, 2).value != "":
+                LOGGER.error(
+                    "sheet %s row %d error: msb not empty in register define row",
+                    sheet.name,
+                    row + 1)
+                raise Exception("msb must be emtpy.")
+            if sheet.cell(row, 3).value != "":
+                LOGGER.error(
+                    "sheet %s row %d error: lsb not empty in register define row",
+                    sheet.name,
+                    row + 1)
+                raise Exception("lsb must be empty")
+            if sheet.cell(row, 4).value != "":
+                LOGGER.error(
+                    "sheet %s row %d error: field not empty in register define row",
+                    sheet.name,
+                    row + 1)
+                raise Exception("field must be empty")
+            if sheet.cell(row, 5).value != "":
+                LOGGER.error(
+                    "sheet %s row %d error: access not empty in register define row",
+                    sheet.name,
+                    row + 1)
+                raise Exception("access must be empty")
+            if sheet.cell(row, 6).value != "":
+                LOGGER.error(
+                    "sheet %s row %d error: Default value not empty in register define row",
+                    sheet.name,
+                    row + 1)
+                raise Exception("default must be empty")
+            register = block.find_register_by_name(name)
+            row = row + 1
+            lsb_pre = -1
+            while sheet.cell(row, 2).value != "":
+                msb = int(sheet.cell(row, 2).value)
+                lsb = int(sheet.cell(row, 3).value)
+                field_name = sheet.cell(row, 4).value
+                access = sheet.cell(row, 5).value.upper()
+                default = sheet.cell(row, 6).value
                 description = "%s" % (sheet.cell(row, 7).value)
-                register = Register(name, offset, description)
-                block.add_register(register)
-                if sheet.cell(row, 2).value != "":
-                    LOGGER.error(
-                        "sheet %s row %d error: msb not empty in register define row",
-                        sheet.name,
-                        row + 1)
-                    raise Exception("msb must be emtpy.")
-                if sheet.cell(row, 3).value != "":
-                    LOGGER.error(
-                        "sheet %s row %d error: lsb not empty in register define row",
-                        sheet.name,
-                        row + 1)
-                    raise Exception("lsb must be empty")
-                if sheet.cell(row, 4).value != "":
-                    LOGGER.error(
-                        "sheet %s row %d error: field not empty in register define row",
-                        sheet.name,
-                        row + 1)
-                    raise Exception("field must be empty")
-                if sheet.cell(row, 5).value != "":
-                    LOGGER.error(
-                        "sheet %s row %d error: access not empty in register define row",
-                        sheet.name,
-                        row + 1)
-                    raise Exception("access must be empty")
-                if sheet.cell(row, 6).value != "":
-                    LOGGER.error(
-                        "sheet %s row %d error: Default value not empty in register define row",
-                        sheet.name,
-                        row + 1)
-                    raise Exception("default must be empty")
-                register = block.find_register_by_name(name)
-                row = row + 1
-                lsb_pre = -1
-                while sheet.cell(row, 2).value != "":
-                    msb = int(sheet.cell(row, 2).value)
-                    lsb = int(sheet.cell(row, 3).value)
-                    field_name = sheet.cell(row, 4).value
-                    access = sheet.cell(row, 5).value.upper()
-                    default = sheet.cell(row, 6).value
-                    description = "%s" % (sheet.cell(row, 7).value)
 
-                    if msb not in range(block.data_len):
-                        LOGGER.error(
-                            "sheet %s row %d error: invalid msb %d",
-                            sheet.name,
-                            row + 1,
-                            msb)
-                        raise Exception("Invalid msb.")
-                    if lsb not in range(block.data_len):
-                        LOGGER.error(
-                            "sheet %s row %d error: invalid lsb %d",
-                            sheet.name,
-                            row + 1,
-                            lsb)
-                        raise Exceptioin("Invalid lsb")
-                    if lsb > msb:
-                        LOGGER.error(
-                            "sheet %s row %d error: lsb > msb",
-                            sheet.name,
-                            row + 1)
-                        raise Exception("lsb > msb")
-                    if lsb_pre >= msb:
-                        LOGGER.error(
-                            "sheet %s row %d error: previous lsb > current msb",
-                            sheet.name,
-                            row + 1)
-                        raise Exception("previous lsb > current msb")
-                    if field_name == "":
-                        LOGGER.error(
-                            "sheet %s row %d error: no Field Name",
-                            sheet.name,
-                            row + 1)
-                        raise Exception("no Field Name")
-                    if access not in RW_TYPES:
-                        LOGGER.error(
-                            "sheet %s row %d error: invalid access type",
-                            sheet.name,
-                            row + 1)
-                        raise Exception("Invalid access type.")
-                    if re.match(r"0x", str(default)):
-                        default = int(default, 16)
-                    else:
-                        try:
-                            default = int(default)
-                        except:
-                            LOGGER.error(
-                                "sheet %s row %d error: invalid default value",
-                                sheet.name,
-                                row + 1)
-                            raise Exception("Invalid default value")
-                    if default >= (1 << (msb-lsb + 1)):
-                        LOGGER.error(
-                            "sheet %s row %d error: Default value is out of range",
-                            sheet.name,
-                            row + 1)
-                        raise Exception("Default value is out of range.")
-                    field = Field(field_name, msb, lsb, default, access, description)
-                    register.add_field(field)
-                    lsb_pre = lsb
-                    if row < sheet.nrows - 1:
-                        row = row + 1
-                    else:
-                        break
-
-                if (re.match(r"0x", str(sheet.cell(row, 0).value))):
-                    LOGGER.debug(
-                        "sheet %s row %d error: no blank row between registers",
+                if msb not in range(block.data_len):
+                    LOGGER.error(
+                        "sheet %s row %d error: invalid msb %d",
+                        sheet.name,
+                        row + 1,
+                        msb)
+                    raise Exception("Invalid msb.")
+                if lsb not in range(block.data_len):
+                    LOGGER.error(
+                        "sheet %s row %d error: invalid lsb %d",
+                        sheet.name,
+                        row + 1,
+                        lsb)
+                    raise Exceptioin("Invalid lsb")
+                if lsb > msb:
+                    LOGGER.error(
+                        "sheet %s row %d error: lsb > msb",
                         sheet.name,
                         row + 1)
-                    raise Exception("No blank row between registers")
-                row = row + 1
-                del register
-            else:
-                if sheet.cell(row, 0).value != "":
+                    raise Exception("lsb > msb")
+                if lsb_pre >= msb:
                     LOGGER.error(
-                        "sheet %s row %d error: None emtpy row %d",
+                        "sheet %s row %d error: previous lsb > current msb",
                         sheet.name,
-                        row)
-                    LOGGER.error(" %s", sheet.cell(row, 0).value)
-                    raise Exception("None empty row.")
-                row = row + 1
+                        row + 1)
+                    raise Exception("previous lsb > current msb")
+                if field_name == "":
+                    LOGGER.error(
+                        "sheet %s row %d error: no Field Name",
+                        sheet.name,
+                        row + 1)
+                    raise Exception("no Field Name")
+                if access not in RW_TYPES:
+                    LOGGER.error(
+                        "sheet %s row %d error: invalid access type",
+                        sheet.name,
+                        row + 1)
+                    raise Exception("Invalid access type.")
+                if re.match(r"0x", str(default)):
+                    default = int(default, 16)
+                else:
+                    try:
+                        default = int(default)
+                    except:
+                        LOGGER.error(
+                            "sheet %s row %d error: invalid default value",
+                            sheet.name,
+                            row + 1)
+                        raise Exception("Invalid default value")
+                if default >= (1 << (msb-lsb + 1)):
+                    LOGGER.error(
+                        "sheet %s row %d error: Default value is out of range",
+                        sheet.name,
+                        row + 1)
+                    raise Exception("Default value is out of range.")
+                field = Field(field_name, msb, lsb, default, access, description)
+                register.add_field(field)
+                lsb_pre = lsb
+                if row < sheet.nrows - 1:
+                    row = row + 1
+                else:
+                    break
+
+            if (re.match(r"0x", str(sheet.cell(row, 0).value))):
+                LOGGER.debug(
+                    "sheet %s row %d error: no blank row between registers",
+                    sheet.name,
+                    row + 1)
+                raise Exception("No blank row between registers")
+            row = row + 1
+            del register
+        else:
+            if sheet.cell(row, 0).value != "":
+                LOGGER.error(
+                    "sheet %s row %d error: None emtpy row %d",
+                    sheet.name,
+                    row)
+                LOGGER.error(" %s", sheet.cell(row, 0).value)
+                raise Exception("None empty row.")
+            row = row + 1
     block.sort_register_by_offset()
     LOGGER.debug(
         "Processing sheet %s done",
