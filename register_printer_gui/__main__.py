8e1eaf2dcdcd91cae14529414ba8f02f8b500f65
Yi Zhang <zhangyiant@hotmail.com>
2019-11-10 16:27:20 +0800

    Connect signals for open path.

--- register_printer_gui/__main__.py
+++ register_printer_gui/__main__.py
@@ -2,7 +2,8 @@ import sys
 from PySide2.QtCore import Slot
 from PySide2.QtWidgets import (
     QApplication,
-    QMainWindow
+    QMainWindow,
+    QFileDialog
 )
 from .ui_mainwindow import Ui_MainWindow
 
@@ -13,11 +14,31 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.logging_editor.append("Hello")
         self.ui.pushButton.clicked.connect(self.update_logging)
+        self.ui.config_file_button.clicked.connect(self.select_config_file)
+        self.ui.excel_path_button.clicked.connect(self.select_excel_path)
         return
 
     @Slot()
     def update_logging(self):
         self.ui.logging_editor.append(" World!")
+        return
+
+    @Slot()
+    def select_config_file(self):
+        filename = QFileDialog.getOpenFileName(
+            self,
+            "Choose a config file",
+            filter="Excel files (*.xlsx)")
+        print(filename)
+        return
+
+    @Slot()
+    def select_excel_path(self):
+        path = QFileDialog.getExistingDirectory(
+            self,
+            "Choose input excel path"
+        )
+        print(path)
         return
 
 def main():
