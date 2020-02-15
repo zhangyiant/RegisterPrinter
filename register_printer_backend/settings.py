159dc03acac3abaa6a1228deab5ad54c950ea20d
Yi Zhang <zhangyiant@hotmail.com>
2020-02-15 13:21:28 +0800

    Set allowed_hosts to *.

--- register_printer_backend/settings.py
+++ register_printer_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#)hjv9j26p30%&0y&9053fzuulzse4m*8x=n@crdje7z((t1^9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
