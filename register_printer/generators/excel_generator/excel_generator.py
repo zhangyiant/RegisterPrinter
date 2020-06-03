87284d0ad4d3fa840c72729aa8715a857f8a7ed9
Yi Zhang <zhangyiant@hotmail.com>
2020-06-03 21:53:35 +0800

    Generate top meta info in excel.

--- register_printer/generators/excel_generator/excel_generator.py
+++ register_printer/generators/excel_generator/excel_generator.py
@@ -1,4 +1,7 @@
 import logging
+import os.path
+
+from openpyxl import Workbook
 
 
 LOGGER = logging.getLogger(__name__)
@@ -8,9 +11,40 @@ class ExcelGenerator:
 
     def __init__(self, top_sys, output_path="."):
         self.top_sys = top_sys
+        self.output_path = output_path
         return
 
     def generate(self):
         LOGGER.debug("Generating Excel files...")
 
+        self.generate_top()
+
+        return
+
+    def generate_top(self):
+        filename = os.path.join(
+            self.output_path,
+            "top.xlsx"
+        )
+
+        LOGGER.debug(
+            "Generating Top excel to %s",
+            filename)
+
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Top"
+
+        ws["A1"] = "Top"
+        ws["B1"] = "Top_Module"
+        ws["A2"] = "AddrWidth"
+        ws["B2"] = self.top_sys.addr_width
+        ws["A3"] = "DataWidth"
+        ws["B3"] = self.top_sys.data_width
+        ws["A4"] = "Author"
+        ws["B4"] = self.top_sys.author
+        ws["A5"] = "Version"
+        ws["B5"] = self.top_sys.version
+
+        wb.save(filename=filename)
         return
