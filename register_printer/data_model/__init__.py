7f5b55c6f0d06d4ce10d129d25776f186eb0ca7d
Yi Zhang <zhangyiant@hotmail.com>
2020-06-15 21:34:27 +0800

    Add BlockInstance class.

--- register_printer/data_model/__init__.py
+++ register_printer/data_model/__init__.py
@@ -2,11 +2,13 @@ from .top_sys import TopSys
 from .block import Block
 from .register import Register
 from .field import Field
+from .block_instance import BlockInstance
 
 
 __all__ = [
     "TopSys",
     "Block",
     "Register",
-    "Field"
+    "Field",
+    "BlockInstance"
 ]
---
+++ register_printer/data_model/block_instance.py
@@ -0,0 +1,18 @@
+class BlockInstance:
+
+    def __init__(self,
+                 parent,
+                 name,
+                 block,
+                 base_address,
+                 block_size,
+                 addr_width=None,
+                 data_width=None):
+        self.parent = parent
+        self.name = name
+        self.block = block
+        self.base_address = base_address
+        self.block_size = block_size
+        self.addr_width = addr_width
+        self.data_width = data_width
+        return
