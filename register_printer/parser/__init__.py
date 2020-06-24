4aa37e48f699043662042f939144b5f20cc71e1b
Yi Zhang <zhangyiant@hotmail.com>
2020-06-24 23:13:22 +0800

    Move top sys sheet parser to parser module.

---
+++ register_printer/parser/__init__.py
@@ -0,0 +1,6 @@
+from .top_sys_sheet_parser import parse_top_sys_sheet
+
+
+__all__ = [
+    "parse_top_sys_sheet"
+]
---
+++ register_printer/parser/top_sys_sheet_parser.py
@@ -0,0 +1,59 @@
+import logging
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parse_top_sys_meta_data(sheet):
+    name = sheet.cell(0, 1).value.strip()
+    addr_size = int(sheet.cell(1, 1).value)
+    data_size = int(sheet.cell(2, 1).value)
+    author = sheet.cell(3, 1).value.strip()
+    version = sheet.cell(4, 1).value
+    result = {
+        "name": name,
+        "author": author,
+        "version": version,
+        "default_addr_width": addr_size,
+        "default_data_width": data_size
+    }
+    return result
+
+
+def parse_block_instance_row(row):
+    block_instance_name = row[0].value.strip()
+    block_type = row[1].value.strip()
+    block_base_address = int(row[2].value.strip(), 16)
+    block_size = int(row[3].value.strip(), 16)
+    addr_width = None
+    value = row[4].value
+    if value != "":
+        addr_width = int(value)
+    data_width = None
+    value = row[5].value
+    if value != "":
+        data_width = int(value)
+    result = {
+        "name": block_instance_name,
+        "type": block_type,
+        "base_address": block_base_address,
+        "size": block_size,
+        "addr_width": addr_width,
+        "data_width": data_width
+    }
+    return result
+
+
+def parse_top_sys_sheet(sheet):
+    """
+    Parse top sys excel sheet.
+    :type sheet: xlrd.sheet
+    """
+    top_dict = parse_top_sys_meta_data(sheet)
+
+    top_dict["block_instances"] = []
+    for rowx in range(7, sheet.nrows):
+        row = sheet.row(rowx)
+        block_inst_dict = parse_block_instance_row(row)
+        top_dict["block_instances"].append(block_inst_dict)
+    return top_dict
