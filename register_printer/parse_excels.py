9568356d912b82e191ed32c4c3433c16e023c1e2
Yi Zhang <zhangyiant@hotmail.com>
2020-07-24 15:24:36 +0800

    Generate block_template_list when parsing sheet.

--- register_printer/parse_excels.py
+++ register_printer/parse_excels.py
@@ -204,14 +204,18 @@ def parse_excel_file(filename, top_sys, top_sys_dict):
     LOGGER.debug("Block types need to be checked: %s.", block_types)
     sheet_list = get_sheet_list(workbook, block_types)
 
+    block_template_list = []
     for sheet in sheet_list:
         block_template = generate_block_template_from_sheet(
             sheet
         )
-        block = top_sys.find_block_by_type(sheet.name)
+        block_template_list.append(block_template)
+
+    for block_template in block_template_list:
+        block_type = block_template.block_type
+        block = top_sys.find_block_by_type(block_type)
         if block is not None:
             block.block_template = block_template
-
     return
 
 
