adf6a3dad1a354ef7a1c273aaba71258b63beefb
Yi Zhang <zhangyiant@hotmail.com>
2020-07-24 15:57:10 +0800

    parse_excels only return block_template_list.

--- register_printer/parse_config.py
+++ register_printer/parse_config.py
@@ -9,10 +9,21 @@ from .parser import parse_top_sys_file
 LOGGER = logging.getLogger(__name__)
 
 
+def update_top_sys_block_template(block_template_list, top_sys):
+    for block_template in block_template_list:
+        block_type = block_template.block_type
+        block = top_sys.find_block_by_type(block_type)
+        if block is not None:
+            block.block_template = block_template
+    return
+
+
 def parse_top_sys(config_file, excel_path):
     top_sys_dict = parse_top_sys_file(config_file)
     top_sys = TopSys.from_top_sys_dict(top_sys_dict)
-    top_sys = parse_excels(top_sys, excel_path, top_sys_dict)
+
+    block_template_list = parse_excels(top_sys, excel_path, top_sys_dict)
+    update_top_sys_block_template(block_template_list, top_sys)
 
     for blk in top_sys.blocks:
         if len(blk.registers) == 0:
--- register_printer/parse_excels.py
+++ register_printer/parse_excels.py
@@ -16,20 +16,12 @@ def get_block_types(top_sys_dict):
     return block_types
 
 
-def update_top_sys_block_template(block_template_list, top_sys):
-    for block_template in block_template_list:
-        block_type = block_template.block_type
-        block = top_sys.find_block_by_type(block_type)
-        if block is not None:
-            block.block_template = block_template
-    return
-
-
 def parse_excels(top_sys, work_path, top_sys_dict):
 
     LOGGER.debug("Parsing with excel files in %s.", work_path)
 
     filenames = os.listdir(work_path)
+    block_template_list = []
     for filename in filenames:
         if re.match(r'~', filename):
             continue
@@ -38,7 +30,7 @@ def parse_excels(top_sys, work_path, top_sys_dict):
             LOGGER.info("Parsing excels file: %s", full_filename)
             block_types = get_block_types(top_sys_dict)
             LOGGER.debug("Block types need to be checked: %s.", block_types)
-            block_template_list = parse_block_template_file(full_filename, block_types)
-            update_top_sys_block_template(block_template_list, top_sys)
+            temp_block_template_list = parse_block_template_file(full_filename, block_types)
+            block_template_list.extend(temp_block_template_list)
+    return block_template_list
 
-    return top_sys
