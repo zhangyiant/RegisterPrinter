f766c4365af741f0c5d27c8d1df6e310d9a50ab4
Yi Zhang <zhangyiant@hotmail.com>
2020-12-21 09:50:32 +0800

    Remove license generator.

--- license_generator.py
+++
@@ -1,9 +0,0 @@
-from register_printer.license import (
-    generate_license)
-
-unique_id = input("Please input your ID: ")
-license_content = generate_license(unique_id.strip())
-with open("RegisterPrinterLicense.txt", "w") as f:
-    f.write(license_content)
-
-    
