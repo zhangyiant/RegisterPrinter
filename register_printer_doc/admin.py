ebd20396290dbdacdb65908e116cbb2e33a9a838
Yi Zhang <zhangyiant@hotmail.com>
2021-01-25 19:57:03 +0800

    Move backend files to register-printer-backend repository.

--- register_printer_doc/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import RegisterPrinterDoc
-
-admin.site.register(RegisterPrinterDoc)
--- register_printer_doc/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class RegisterPrinterDoc(models.Model):
-    rp_doc_id = models.AutoField(
-        primary_key=True)
-    name = models.CharField(
-        max_length=255)
-    doc = models.TextField()
-    
