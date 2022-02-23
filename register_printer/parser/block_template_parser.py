9c499e7d2d8e3ea7b8d0353da4846d4c00d30e8e
Yi Zhang <zhangyiant@hotmail.com>
2022-02-23 21:20:56 +0800

    Support parse array table.

--- register_printer/parser/block_template_parser.py
+++ register_printer/parser/block_template_parser.py
@@ -58,6 +58,93 @@ def find_register_table_title_row(sheet, previous_context):
         raise ExcelParseException(msg, context)
     return rowx
 
+def find_array_table_title_row(sheet, previous_context):
+    context = previous_context.copy()
+    rowx = 1
+    context.row = rowx
+    found = False
+    while rowx < sheet.nrows:
+        row = sheet.row(rowx)
+        if is_array_table_title_row(row):
+            found = True
+            break
+        else:
+            rowx += 1
+            context.row = rowx
+    if found:
+        return rowx
+    return None
+
+def parse_array_row(row, previous_context):
+    """
+        row: xlrd row object. You can obtain it by sheet.row()
+                 a sequence of cells.
+    """
+    context = previous_context.copy()
+
+    context.column = 0
+    array_name = row[0].value
+
+    context.column = 1
+    try:
+        array_length = int(row[1].value)
+    except Exception as exc:
+        msg = "Parse array length error: {}.".format(exc)
+        raise ExcelParseException(msg, context)
+
+    context.column = 2
+    try:
+        array_offset = int(row[2].value, 16)
+    except Exception as exc:
+        msg = "Parse array offset error: {}.".format(exc)
+        raise ExcelParseException(msg, context)
+
+    context.column = 3
+    try:
+        start_address = int(row[3].value, 16)
+    except Exception as exc:
+        msg = "Parse start address error: {}.".format(exc)
+        raise ExcelParseException(msg, context)
+
+    context.column = 4
+    try:
+        end_address = int(row[4].value, 16)
+    except Exception as exc:
+        msg = "Parse end address error: {}.".format(exc)
+        raise ExcelParseException(msg, context)
+
+    context.column = 5
+    description = row[5].value
+
+    result = {
+        "arrayName": array_name,
+        "arrayLength": array_length,
+        "arrayOffset": array_offset,
+        "startAddress": start_address,
+        "endAddress": end_address,
+        "description": description
+    }
+    return result
+
+def parse_array_table(sheet, start_rowx, previous_context):
+    context = previous_context.copy()
+    rowx = start_rowx
+    context.row = rowx
+
+    array_dict_list = []
+    while rowx < sheet.nrows:
+        row = sheet.row(rowx)
+        context.row = rowx
+        if is_table_title_row(row):
+            break
+        if is_empty_row(row):
+            rowx += 1
+        else:
+            array_dict = parse_array_row(row, context)
+            array_dict_list.append(array_dict)
+            rowx += 1
+    return array_dict_list
+
 def parse_register_table(sheet, start_rowx, previous_context):
     context = previous_context.copy()
     rowx = start_rowx
@@ -120,7 +207,6 @@ def is_register_row(row):
         return True
     return False
 
-
 def generate_block_template_from_sheet(sheet, previous_context):
 
     context = previous_context.copy()
@@ -134,22 +220,38 @@ def generate_block_template_from_sheet(sheet, previous_context):
 
     validate_sheet(sheet, context)
 
+
+    block_template_dict = {
+        "blockType": sheet.name,
+        "registers": [],
+        "arrays": []
+    }
+
     register_table_title_rowx = find_register_table_title_row(
         sheet,
         context
     )
-
     rowx = register_table_title_rowx + 1
     context.row = rowx
-    block_template_dict = {
-        "blockType": sheet.name,
-        "registers": []
-    }
     block_template_dict["registers"] = parse_register_table(
         sheet,
         rowx,
         context
     )
+
+    array_table_title_rowx = find_array_table_title_row(
+        sheet,
+        context
+    )
+    if array_table_title_rowx is not None:
+        rowx = array_table_title_rowx + 1
+        context.row = rowx
+        array_dict_list = parse_array_table(
+            sheet,
+            rowx,
+            context
+        )
+        block_template_dict["arrays"] = array_dict_list
 
     LOGGER.debug(
         "Processing sheet %s done",
