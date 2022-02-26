c9020af50cc7496073d45bd66d6d16022f4f6fa3
Yi Zhang <zhangyiant@hotmail.com>
2022-02-26 21:05:45 +0800

    Rename to ...Template.

--- register_printer/data_model/__init__.py
+++ register_printer/data_model/__init__.py
@@ -1,6 +1,6 @@
 from .top_sys import TopSys
-from .register import Register
-from .field import Field
+from .register_template import RegisterTemplate
+from .field_template import FieldTemplate
 from .block_template import BlockTemplate
 from .block import Block
 from .block_instance import BlockInstance
@@ -8,8 +8,8 @@ from .block_instance import BlockInstance
 
 __all__ = [
     "TopSys",
-    "Register",
-    "Field",
+    "RegisterTemplate",
+    "FieldTemplate",
     "BlockTemplate",
     "Block",
     "BlockInstance"
