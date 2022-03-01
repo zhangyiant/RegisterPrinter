653d4ab7f5be9f1602e964011398b5cc25d48463
Yi Zhang <zhangyiant@hotmail.com>
2022-03-01 19:50:13 +0800

    Move print_c_header_block to a separate file.

--- register_printer/generators/c_header_generator/print_c_header.py
+++ register_printer/generators/c_header_generator/print_c_header.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import logging
 from register_printer.template_loader import get_template
+from .print_c_header_block import print_c_header_block
 
 
 LOGGER = logging.getLogger(__name__)
@@ -27,69 +28,6 @@ def print_c_test(top_sys, out_path):
 
     with open(file_name, "w") as sfh:
         sfh.write(content)
-
-    return
-
-
-def print_c_header_block(block, out_path):
-
-    LOGGER.debug("Print block %s C header...", block.block_type)
-
-    file_name = os.path.join(
-        out_path,
-        "regs_" + block.block_type.lower() + ".h")
-    if os.path.exists(file_name):
-        os.remove(file_name)
-
-    struct_fields = []
-    rsvd_idx = 0
-    accumulated_number_rsvd_register = 0
-    for reg in block.mapped_registers:
-        # Todo: reimplement this function
-        if reg.type == "RegisterType.RESERVED":
-            accumulated_number_rsvd_register += 1
-        else:
-            if accumulated_number_rsvd_register > 1:
-                struct_field = {
-                    "type": "volatile const int",
-                    "name": "RSVD%d[%d]" % (rsvd_idx, accumulated_number_rsvd_register)
-                }
-                struct_fields.append(struct_field)
-                rsvd_idx = rsvd_idx + 1
-                # reset accumulated_number_rsvd_register
-                accumulated_number_rsvd_register = 0
-            struct_field = {
-                "type": "volatile int",
-                "name": reg.name.upper()
-            }
-            struct_fields.append(struct_field)
-
-    pos_mask_macros = []
-    for reg in block.mapped_registers:
-        if reg.type != RegisterType.RESERVED:
-            for fld in reg.fields:
-                if fld.name != "-":
-                    prefix = reg.name.upper() + "_" + fld.name.upper()
-                    pos_value = fld.lsb
-                    mask_value = (1 << (fld.msb - fld.lsb + 1)) - 1
-                    pos_mask_macros.append({
-                        "prefix": prefix,
-                        "pos_value": pos_value,
-                        "mask_value": mask_value
-                    })
-
-    template = get_template("c_header_block.h")
-
-    content = template.render(
-        {
-            "block_type": block.block_type,
-            "struct_fields": struct_fields,
-            "pos_mask_macros": pos_mask_macros
-        }
-    )
-
-    with open(file_name, "w") as bfh:
-        bfh.write(content)
 
     return
 
---
+++ register_printer/generators/c_header_generator/print_c_header_block.py
@@ -0,0 +1,70 @@
+import logging
+import os
+
+from register_printer.template_loader import get_template
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def print_c_header_block(block, out_path):
+
+    LOGGER.debug("Print block %s C header...", block.block_type)
+
+    file_name = os.path.join(
+        out_path,
+        "regs_" + block.block_type.lower() + ".h")
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    struct_fields = []
+    rsvd_idx = 0
+    accumulated_number_rsvd_register = 0
+    for reg in block.mapped_registers:
+        # Todo: reimplement this function
+        if reg.type == "RegisterType.RESERVED":
+            accumulated_number_rsvd_register += 1
+        else:
+            if accumulated_number_rsvd_register > 1:
+                struct_field = {
+                    "type": "volatile const int",
+                    "name": "RSVD%d[%d]" % (rsvd_idx, accumulated_number_rsvd_register)
+                }
+                struct_fields.append(struct_field)
+                rsvd_idx = rsvd_idx + 1
+                # reset accumulated_number_rsvd_register
+                accumulated_number_rsvd_register = 0
+            struct_field = {
+                "type": "volatile int",
+                "name": reg.name.upper()
+            }
+            struct_fields.append(struct_field)
+
+    pos_mask_macros = []
+    for reg in block.mapped_registers:
+        if reg.type != RegisterType.RESERVED:
+            for fld in reg.fields:
+                if fld.name != "-":
+                    prefix = reg.name.upper() + "_" + fld.name.upper()
+                    pos_value = fld.lsb
+                    mask_value = (1 << (fld.msb - fld.lsb + 1)) - 1
+                    pos_mask_macros.append({
+                        "prefix": prefix,
+                        "pos_value": pos_value,
+                        "mask_value": mask_value
+                    })
+
+    template = get_template("c_header_block.h")
+
+    content = template.render(
+        {
+            "block_type": block.block_type,
+            "struct_fields": struct_fields,
+            "pos_mask_macros": pos_mask_macros
+        }
+    )
+
+    with open(file_name, "w") as bfh:
+        bfh.write(content)
+
+    return
