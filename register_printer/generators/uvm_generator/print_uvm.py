4cf29d0a9b76a9ab886f3263560026eabab2bac5
Yi Zhang <zhangyiant@hotmail.com>
2022-03-03 07:15:58 +0800

    Move print_uvm_sys to a separate file.

--- register_printer/generators/uvm_generator/print_uvm.py
+++ register_printer/generators/uvm_generator/print_uvm.py
@@ -1,34 +1,12 @@
-import re
 import os
 import os.path
 import logging
 from register_printer.template_loader import get_template
 from .print_uvm_block import print_uvm_block
+from .print_uvm_sys import print_uvm_sys
 
 LOGGER = logging.getLogger(__name__)
 
-
-def print_uvm_sys(top_sys, out_path):
-    uvm_sys_name = top_sys.name.lower() + "_reg_model"
-    file_name = os.path.join(
-        out_path,
-        uvm_sys_name + ".sv")
-
-    if os.path.exists(file_name):
-        os.remove(file_name)
-
-    template = get_template("sys_model.sv")
-
-    content = template.render(
-        {
-            "top_sys": top_sys
-        }
-    )
-
-    with open(file_name, "w") as bfh:
-        bfh.write(content)
-
-    return
 
 def print_sv_defines(top_sys, out_path):
 
