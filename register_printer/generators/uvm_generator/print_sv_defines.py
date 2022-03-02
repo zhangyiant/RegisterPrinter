4a4dec5ce458013fc4b726243cbd65127c3feb4c
Yi Zhang <zhangyiant@hotmail.com>
2022-03-03 07:35:46 +0800

    Print uvm defines.

--- register_printer/generators/uvm_generator/print_sv_defines.py
+++ register_printer/generators/uvm_generator/print_sv_defines.py
@@ -2,10 +2,28 @@ import os
 import os.path
 import logging
 from register_printer.template_loader import get_template
+from register_printer.data_model import Register, Array, Struct
 
 
 LOGGER = logging.getLogger(__name__)
 
+
+def get_instances(block_instances):
+    result = []
+    for block_instance in block_instances:
+        instance_dict = {}
+        instance_dict["name"] = block_instance.name
+        instance_dict["base_address"] = block_instance.base_address
+        instance_dict["registers"] = []
+        for register in block_instance.block.registers:
+            if isinstance(register, Register):
+                if not register.is_reserved:
+                    reg_dict = {}
+                    reg_dict["name"] = register.name
+                    reg_dict["offset"] = register.offset
+                    instance_dict["registers"].append(reg_dict)
+        result.append(instance_dict)
+    return result
 
 def print_sv_defines(top_sys, out_path):
 
@@ -19,9 +37,12 @@ def print_sv_defines(top_sys, out_path):
 
     template = get_template("register_defines.svh")
 
+    instances = get_instances(top_sys.block_instances)
+
     content = template.render(
         {
-            "top_sys": top_sys
+            "sv_def_name": top_sys.name.lower() + "_register_defines",
+            "instances": instances
         }
     )
 
--- register_printer/templates/register_defines.svh
+++ register_printer/templates/register_defines.svh
@@ -1,16 +1,13 @@
-{% set sv_def_name = top_sys.name.lower() + "_register_defines" %}
 `ifndef {{ sv_def_name | upper }}__SVH
 `define {{ sv_def_name | upper }}__SVH
 
-{% for block_instance in top_sys.block_instances %}
-{% set block_type = block_instance.block_type %}
-{% set block_instance_name = block_instance.name.lower() %}
-{% set base_address = block_instance.base_address %}
-    `define {{ block_instance_name | upper }}_BASE_ADDR    'h{{ "%x" | format(base_address) }}
-    {% set block = block_instance.block %}
-    {% for register in block.registers %}
-    {% set block_instance_reg_name = (block_instance_name + '_' + register.name + "_addr").upper() %}
-    `define {{ "%-48s" | format(block_instance_reg_name) }}    `{{ "%-32s" | format(block_instance_name.upper() + "_BASE_ADDR") }} + 'h{{ "%x" | format(register.offset )}}
+{% for instance in instances %}
+{% set block_type = instance.block_type %}
+{% set block_instance_name = instance.name.lower() %}
+    `define {{ instance.name | upper }}_BASE_ADDR    'h{{ "%x" | format(instance.base_address) }}
+    {% for register in instance.registers %}
+    {% set block_instance_reg_name = (instance.name + '_' + register.name + "_addr").upper() %}
+    `define {{ "%-48s" | format(block_instance_reg_name) }}    `{{ "%-32s" | format(instance.name.upper() + "_BASE_ADDR") }} + 'h{{ "%x" | format(register.offset )}}
     {% endfor %}
 
 {% endfor %}
