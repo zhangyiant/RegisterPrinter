ca3d9ea69fe90be1d6eb2c923f68c27044696fc1
Yi Zhang <zhangyiant@hotmail.com>
2022-03-05 11:36:51 +0800

    Refactoring the rtl generator code.

--- register_printer/generators/rtl_generator/print_rtl.py
+++ register_printer/generators/rtl_generator/print_rtl.py
@@ -1,12 +1,68 @@
-import re
 import os
 import os.path
 import logging
 from register_printer.template_loader import get_template
+from register_printer.data_model import Register
 
 
 LOGGER = logging.getLogger(__name__)
 
+
+def get_register_dict_from_register(register):
+    tmp_register = {}
+    tmp_register["name"] = register.name
+    tmp_register["fields"] = []
+    tmp_register["offset"] = register.offset
+    rw_flds = []
+    rwp_flds = []
+    ro_flds = []
+    rc_flds = []
+    rs_flds = []
+    w1c_flds = []
+    w0c_flds = []
+    wc_flds = []
+    wo_flds = []
+    wrc_flds = []
+    wrs_flds = []
+    #rsc_flds = []
+    for fld in register.fields:
+        tmp_register["fields"].append(fld)
+        if fld.access == "RW":
+            rw_flds.append(fld)
+        elif fld.access == "RWP":
+            rwp_flds.append(fld)
+        elif fld.access == "RC":
+            rc_flds.append(fld)
+        elif fld.access == "RO":
+            ro_flds.append(fld)
+        elif fld.access == "RS":
+            rs_flds.append(fld)
+        elif fld.access == "W1C":
+            w1c_flds.append(fld)
+        elif fld.access == "W0C":
+            w0c_flds.append(fld)
+        elif fld.access == "WC":
+            wc_flds.append(fld)
+        elif fld.access == "WO":
+            wo_flds.append(fld)
+        elif fld.access == "WRC":
+            wrc_flds.append(fld)
+        elif fld.access == "WRS":
+            wrs_flds.append(fld)
+        elif fld.access == "-":
+            ro_flds.append(fld)
+    tmp_register["rw_flds"] = rw_flds
+    tmp_register["rwp_flds"] = rwp_flds
+    tmp_register["ro_flds"] = ro_flds
+    tmp_register["rc_flds"] = rc_flds
+    tmp_register["rs_flds"] = rs_flds
+    tmp_register["w1c_flds"] = w1c_flds
+    tmp_register["w0c_flds"] = w0c_flds
+    tmp_register["wc_flds"] = wc_flds
+    tmp_register["wo_flds"] = wo_flds
+    tmp_register["wrc_flds"] = wrc_flds
+    tmp_register["wrs_flds"] = wrs_flds
+    return tmp_register
 
 def print_rtl_block(block, out_path):
     file_name = os.path.join(
@@ -22,59 +78,12 @@ def print_rtl_block(block, out_path):
 
     tmp_registers = []
     for reg in block.registers:
-        tmp_register = {}
-        tmp_register["name"] = reg.name
-        tmp_register["fields"] = []
-        rw_flds = []
-        rwp_flds = []
-        ro_flds = []
-        rc_flds = []
-        rs_flds = []
-        w1c_flds = []
-        w0c_flds = []
-        wc_flds = []
-        wo_flds = []
-        wrc_flds = []
-        wrs_flds = []
-        #rsc_flds = []
-        for fld in reg.fields:
-            tmp_register["fields"].append(fld)
-            if fld.access == "RW":
-                rw_flds.append(fld)
-            elif fld.access == "RWP":
-                rwp_flds.append(fld)
-            elif fld.access == "RC":
-                rc_flds.append(fld)
-            elif fld.access == "RO":
-                ro_flds.append(fld)
-            elif fld.access == "RS":
-                rs_flds.append(fld)
-            elif fld.access == "W1C":
-                w1c_flds.append(fld)
-            elif fld.access == "W0C":
-                w0c_flds.append(fld)
-            elif fld.access == "WC":
-                wc_flds.append(fld)
-            elif fld.access == "WO":
-                wo_flds.append(fld)
-            elif fld.access == "WRC":
-                wrc_flds.append(fld)
-            elif fld.access == "WRS":
-                wrs_flds.append(fld)
-            elif fld.access == "-":
-                ro_flds.append(fld)
-        tmp_register["rw_flds"] = rw_flds
-        tmp_register["rwp_flds"] = rwp_flds
-        tmp_register["ro_flds"] = ro_flds
-        tmp_register["rc_flds"] = rc_flds
-        tmp_register["rs_flds"] = rs_flds
-        tmp_register["w1c_flds"] = w1c_flds
-        tmp_register["w0c_flds"] = w0c_flds
-        tmp_register["wc_flds"] = wc_flds
-        tmp_register["wo_flds"] = wo_flds
-        tmp_register["wrc_flds"] = wrc_flds
-        tmp_register["wrs_flds"] = wrs_flds
-        tmp_registers.append(tmp_register)
+        if isinstance(reg, Register):
+            if not reg.is_reserved:
+                register_dict = get_register_dict_from_register(
+                    reg
+                )
+                tmp_registers.append(register_dict)
 
     content = template.render(
         {
