f4814078f81e953a5bf8d93cf9580d9642970ff1
Yi Zhang <zhangyiant@hotmail.com>
2022-03-26 19:46:39 +0800

    Refactor test_c_generator() method.

--- register_printer/tests/test_register_printer.py
+++ register_printer/tests/test_register_printer.py
@@ -122,85 +122,28 @@ class TestRegisterPrinter(TestCase):
                 "regheaders"
             )
 
-            # Compare top module header file
-            top_module_filename = os.path.join(
-                reg_headers_file_path, "regs_top_module.h")
-            self.assertTrue(
-                os.path.exists(top_module_filename),
-                "Top module header file is not generated."
-            )
-            baseline_top_module_filename = os.path.join(
+            compare_files = [
+                "regs_top_module.h",
+                "regs_type1.h",
+                "regs_type2.h",
+                "test.c"
+            ]
+
+            (match_list, mismatch_list, error_list) = filecmp.cmpfiles(
+                reg_headers_file_path,
                 baseline_reg_headers_file_path,
-                "regs_top_module.h"
-            )
-            self.assertTrue(
-                filecmp.cmp(
-                    top_module_filename,
-                    baseline_top_module_filename
-                ),
-                "Top module header file content is not correct."
+                compare_files
             )
 
-            # Compare block header files
-            type1_header_filename = os.path.join(
-                reg_headers_file_path,
-                "regs_type1.h"
-            )
-            baseline_type1_header_filename = os.path.join(
-                baseline_reg_headers_file_path,
-                "regs_type1.h"
+            self.assertTrue(
+                len(mismatch_list) == 0,
+                "C generator files mismatched: {}".format(mismatch_list)
             )
             self.assertTrue(
-                os.path.exists(type1_header_filename),
-                "Type1 header file is not generated."
-            )
-            self.assertTrue(
-                filecmp.cmp(
-                    type1_header_filename,
-                    baseline_type1_header_filename
-                ),
-                "Type1 file content is not correct."
-            )
-            type2_header_filename = os.path.join(
-                reg_headers_file_path,
-                "regs_type2.h"
-            )
-            baseline_type2_header_filename = os.path.join(
-                baseline_reg_headers_file_path,
-                "regs_type2.h"
-            )
-            self.assertTrue(
-                os.path.exists(type2_header_filename),
-                "Type2 header file is not generated."
-            )
-            self.assertTrue(
-                filecmp.cmp(
-                    type2_header_filename,
-                    baseline_type2_header_filename
-                ),
-                "Type2 file content is not correct."
+                len(error_list) == 0,
+                "C generator files errored: {}".format(error_list)
             )
 
-            # compare test.c file
-            test_c_filename = os.path.join(
-                reg_headers_file_path,
-                "test.c"
-            )
-            baseline_test_c_filename = os.path.join(
-                baseline_reg_headers_file_path,
-                "test.c"
-            )
-            self.assertTrue(
-                os.path.exists(test_c_filename),
-                "test.c is not generated."
-            )
-            self.assertTrue(
-                filecmp.cmp(
-                    test_c_filename,
-                    baseline_test_c_filename
-                ),
-                "Test.c file content is not correct."
-            )
         return
 
     def test_doc_generator(self):
