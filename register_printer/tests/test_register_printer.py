7cc60040f66ac79037a46964db60e5d46212ec0b
Yi Zhang <zhangyiant@hotmail.com>
2022-03-26 16:41:00 +0800

    Set DATASET_PATH.

--- register_printer/tests/test_register_printer.py
+++ register_printer/tests/test_register_printer.py
@@ -1,6 +1,18 @@
+import os.path
+from tempfile import TemporaryDirectory
 from unittest import TestCase
+
+DATASET_ROOT_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "dataset"
+)
 
 
 class TestRegisterPrinter(TestCase):
+
+    DATASET_PATH = os.path.join(DATASET_ROOT_PATH, "dataset1")
+
     def test_c_generator(self):
-        pass
+        with TemporaryDirectory() as tmp_dir:
+            self.assertTrue(os.path.exists(tmp_dir), "Tmp directory does not exist")
+
