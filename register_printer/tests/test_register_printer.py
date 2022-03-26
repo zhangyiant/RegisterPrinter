ab93f26475fe45c8eb86d5e38b3ead76dbc8f9e8
Yi Zhang <zhangyiant@hotmail.com>
2022-03-26 16:58:52 +0800

    Check top module C header existence.

--- register_printer/tests/test_register_printer.py
+++ register_printer/tests/test_register_printer.py
@@ -1,5 +1,6 @@
 import os.path
 from tempfile import TemporaryDirectory
+from ..register_printer import RegisterPrinter
 from unittest import TestCase
 
 DATASET_ROOT_PATH = os.path.join(
@@ -13,6 +14,28 @@ class TestRegisterPrinter(TestCase):
     DATASET_PATH = os.path.join(DATASET_ROOT_PATH, "dataset1")
 
     def test_c_generator(self):
+        config_file = os.path.join(
+            TestRegisterPrinter.DATASET_PATH,
+            "abc.xlsx"
+        )
+        excel_path = os.path.join(
+            TestRegisterPrinter.DATASET_PATH,
+            "excels"
+        )
         with TemporaryDirectory() as tmp_dir:
-            self.assertTrue(os.path.exists(tmp_dir), "Tmp directory does not exist")
+            output_path = tmp_dir
+            register_printer = RegisterPrinter(
+                config_file=config_file,
+                excel_path=excel_path,
+                output_path=output_path
+            )
+            register_printer.generate_c_header()
+
+            reg_headers_file_path = os.path.join(tmp_dir, "regheaders")
+            top_module_filename = os.path.join(reg_headers_file_path, "regs_top_module.h")
+            self.assertTrue(
+                os.path.exists(top_module_filename),
+                "Top module header file is not generated."
+            )
+        return
 
