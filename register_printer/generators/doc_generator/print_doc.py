0554d1cc29c6ae7d97247c99ecb0d03aaaa4b01c
Yi Zhang <zhangyiant@hotmail.com>
2022-08-02 22:09:43 +0800

    Fixed offset overwrite.

--- register_printer/generators/doc_generator/print_doc.py
+++ register_printer/generators/doc_generator/print_doc.py
@@ -3,8 +3,7 @@ import os.path
 import logging
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from register_printer.data_model import Register, Array
-
+from register_printer.data_model import Register, Array, Field
 
 LOGGER = logging.getLogger(__name__)
 
@@ -33,6 +32,36 @@ def print_doc_reg(reg, dh, reg_idx, blk_idx, blk_insts, array=None):
 
     p.add_run("    Reset Value : ").bold = True
     p.add_run("0x%x\n" % (reg.calculate_register_default()))
+    if array is not None:
+        for index in range(array.length):
+            offset = array.start_address + array.offset * index + reg.offset
+            temp_reg = Register(offset)
+            temp_reg.name = reg.name
+            for field in reg.fields:
+                temp_reg.fields.append(field)
+            found = False
+            for overwrite_entry in array.default_overwrite_entries:
+                if overwrite_entry.index == index and \
+                        overwrite_entry.register_name == temp_reg.name:
+                    found = True
+                    field_name = overwrite_entry.field_name
+                    for field in temp_reg.fields:
+                        if field.name == field_name:
+                            temp_reg.fields.remove(field)
+                            new_field = Field()
+                            new_field.name = field.name
+                            new_field.msb = field.msb
+                            new_field.lsb = field.lsb
+                            new_field.default = overwrite_entry.default
+                            new_field.access = field.access
+                            new_field.description = field.description
+                            temp_reg.fields.append(new_field)
+            if found:
+                p.add_run(
+                    "           " +
+                    hex(temp_reg.offset) + ":0x%x\n" %
+                    temp_reg.calculate_register_default()
+                )
 
     p.add_run("    Description : ").bold = True
     p.add_run("%s\n" % (reg.description))
@@ -129,7 +158,7 @@ def print_doc_block(doc, idx, block, instances):
         if isinstance(register, Register):
             tb.cell(i, 0).text = hex(register.offset)
             for k in range(len(instances)):
-                tb.cell(i, k+1).text = hex(
+                tb.cell(i, k + 1).text = hex(
                     instances[k].base_address + register.offset)
             tb.cell(i, len(hdr) - 1).text = str(register.name)
             i += 1
