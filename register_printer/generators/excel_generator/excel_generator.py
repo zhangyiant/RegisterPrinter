dcbc80619bd385d7d049f8e3d3c7f09dda94da0f
xxr000 <darkw2022@outlook.com>
2023-08-11 15:09:20 +0800

    update

--- register_printer/generators/excel_generator/excel_generator.py
+++ register_printer/generators/excel_generator/excel_generator.py
@@ -7,6 +7,21 @@ from openpyxl import Workbook, load_workbook
 
 LOGGER = logging.getLogger(__name__)
 
+def get_excel_path(name):
+    if hasattr(sys, 'frozen') and hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS,"register_printer/excels/" + name)
+    return os.path.join(os.path.abspath("."),"register_printer/excels/" + name) 
+
+def copy_cell_style(src_cell,tar_cell):
+    tar_cell.value = src_cell.value
+    if src_cell.has_style:
+        tar_cell._style = copy(src_cell._style)
+        tar_cell.font = copy(src_cell.font)
+        tar_cell.border = copy(src_cell.border)
+        tar_cell.fill= copy(src_cell.fill)
+        tar_cell.number_format = copy(src_cell.number_format)
+        tar_cell.protection = copy(src_cell.protection)
+        tar_cell.alignment = copy(src_cell.alignment)
 
 class ExcelGenerator:
 
@@ -15,21 +30,7 @@ class ExcelGenerator:
         self.output_path = output_path
         return
 
-    def _get_excel_path(self,name):
-        if hasattr(sys, 'frozen') and hasattr(sys, '_MEIPASS'):
-            return os.path.join(sys._MEIPASS,"register_printer/excels/" + name)
-        return os.path.join(os.path.abspath("."),"register_printer/excels/" + name) 
 
-    def _copy_cell_style(self,src_cell,tar_cell):
-        tar_cell.value = src_cell.value
-        if src_cell.has_style:
-            tar_cell._style = copy(src_cell._style)
-            tar_cell.font = copy(src_cell.font)
-            tar_cell.border = copy(src_cell.border)
-            tar_cell.fill= copy(src_cell.fill)
-            tar_cell.number_format = copy(src_cell.number_format)
-            tar_cell.protection = copy(src_cell.protection)
-            tar_cell.alignment = copy(src_cell.alignment)
 
 
     def generate(self):
@@ -65,48 +66,47 @@ class ExcelGenerator:
             "Generate file: %s for block template %s",
             filename,
             block_template.block_type)
+        
+        rb = load_workbook(get_excel_path('block.xlsx'))
+        rs = rb.active
 
         wb = Workbook()
         ws = wb.active
-
+        ws.page_setup = rs.page_setup
         ws.title = block_template.block_type
 
+        for row in range(1,5):
+            for col, value in enumerate(rs[row]):
+                src_cell = rs.cell(row,col+1)
+                tar_cell = ws.cell(row,col+1)
+                copy_cell_style(src_cell,tar_cell)
+        ws.merge_cells("A1:B1")
+        ws.merge_cells("C1:H1")
+        ws.merge_cells("A3:B3")
+
         current_row = 1
-        ws.cell(1, 1).value = "Module description"
-
-        current_row += 2
-        ws.cell(current_row, 1).value = "array description"
-
-        current_row += 1
-        ws.cell(current_row, 1).value = "array_name"
-        ws.cell(current_row, 2).value = "array_len"
-        ws.cell(current_row, 3).value = "array_offset"
-        ws.cell(current_row, 4).value = "start_addr"
-        ws.cell(current_row, 5).value = "end_addr"
-
-        current_row += 1
+        current_row += 4
         for array_template in block_template.array_templates:
-            ws.cell(current_row, 1).value = array_template.name
-            ws.cell(current_row, 2).value = array_template.length
-            ws.cell(current_row, 3).value = hex(array_template.offset)
+            for col, vaule in enumerate(rs[4]):
+                copy_cell_style(rs.cell(5,col+1),ws.cell(current_row,col+1))
+            ws.cell(current_row, 1).value = hex(array_template.offset)
+            ws.cell(current_row, 2).value = array_template.name
+            ws.cell(current_row, 3).value = array_template.length
             ws.cell(current_row, 4).value = hex(array_template.start_address)
             ws.cell(current_row, 5).value = hex(array_template.end_address)
             current_row += 1
 
         current_row += 2
-        ws.cell(current_row, 1).value = "register description"
+        copy_cell_style(rs.cell(7,1),ws.cell(current_row,1))
+        ws.merge_cells(f"A{current_row}:B{current_row}")
 
         current_row += 1
-        ws.cell(current_row, 1).value = "Offset"
-        ws.cell(current_row, 2).value = "Name"
-        ws.cell(current_row, 3).value = "MSB"
-        ws.cell(current_row, 4).value = "LSB"
-        ws.cell(current_row, 5).value = "Field Name"
-        ws.cell(current_row, 6).value = "Access"
-        ws.cell(current_row, 7).value = "Default"
-        ws.cell(current_row, 8).value = "Description"
+        for col, value in enumerate(rs[8]):
+            copy_cell_style(rs.cell(8,col+1),ws.cell(current_row,col+1))
         current_row += 1
         for register in block_template.register_templates:
+            for col, vaule in enumerate(rs[8]):
+                copy_cell_style(rs.cell(9,col+1),ws.cell(current_row,col+1))
             offset_cell = ws.cell(current_row, 1)
             offset_cell.value = hex(register.offset)
             name_cell = ws.cell(current_row, 2)
@@ -115,6 +115,8 @@ class ExcelGenerator:
             description_cell.value = register.description
             current_row += 1
             for field in register.fields:
+                for col, vaule in enumerate(rs[8]):
+                    copy_cell_style(rs.cell(10,col+1),ws.cell(current_row,col+1))
                 msb_cell = ws.cell(current_row, 3)
                 msb_cell.value = field.msb
                 lsb_cell = ws.cell(current_row, 4)
@@ -129,6 +131,8 @@ class ExcelGenerator:
                 field_description_cell.value = field.description
                 current_row += 1
             # Add an empty line
+            for col, vaule in enumerate(rs[8]):
+                copy_cell_style(rs.cell(11,col+1),ws.cell(current_row,col+1))
             current_row += 1
         wb.save(filename=filename)
 
@@ -144,7 +148,7 @@ class ExcelGenerator:
             "Generating Top excel to %s",
             filename)
 
-        rb = load_workbook(self._get_excel_path('top.xlsx'))
+        rb = load_workbook(get_excel_path('top.xlsx'))
         rs = rb.active
 
         wb = Workbook()
@@ -157,7 +161,7 @@ class ExcelGenerator:
             for col, value in enumerate(rs[row]):
                 src_cell = rs.cell(row,col+1)
                 tar_cell = ws.cell(row,col+1)
-                self._copy_cell_style(src_cell,tar_cell)
+                copy_cell_style(src_cell,tar_cell)
 
         ws["B1"] = "Top_Module"
         ws["B2"] = self.top_sys.addr_width
@@ -169,26 +173,26 @@ class ExcelGenerator:
         row = 8
         for block_instance in self.top_sys.block_instances:
             name_cell = ws.cell(row, 1)
-            self._copy_cell_style(rs.cell(8+row%2,1),name_cell)
+            copy_cell_style(rs.cell(8+row%2,1),name_cell)
             name_cell.value = block_instance.name
             type_cell = ws.cell(row, 2)
-            self._copy_cell_style(rs.cell(8+row%2,1),type_cell)
+            copy_cell_style(rs.cell(8+row%2,1),type_cell)
             type_cell.value = block_instance.block.block_type
             base_address_cell = ws.cell(row, 3)
-            self._copy_cell_style(rs.cell(8,1),base_address_cell)
+            copy_cell_style(rs.cell(8,1),base_address_cell)
             base_address_cell.value = hex(block_instance.base_address)
             size_cell = ws.cell(row, 4)
-            self._copy_cell_style(rs.cell(8,1),size_cell)
+            copy_cell_style(rs.cell(8,1),size_cell)
             size_cell.value = hex(block_instance.size)
             addr_width_cell = ws.cell(row, 5)
-            self._copy_cell_style(rs.cell(8,1),addr_width_cell)
+            copy_cell_style(rs.cell(8,1),addr_width_cell)
             raw_addr_width = block_instance.block.raw_addr_width
             if raw_addr_width is None:
                 addr_width_cell.value = ""
             else:
                 addr_width_cell.value = raw_addr_width
             data_width_cell = ws.cell(row, 6)
-            self._copy_cell_style(rs.cell(8,1),data_width_cell)
+            copy_cell_style(rs.cell(8,1),data_width_cell)
             raw_data_width = block_instance.block.raw_data_width
             if raw_data_width is None:
                 data_width_cell.value = ""
